f81ff355a8edcd2c12367db5bd853e6b5c83686a
daveh <dave.halladay@outlook.com>
2022-03-05 13:56:56 -0700

    update routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,7 +1,27 @@
 const router = require('express').Router();
-
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  deleteFriend
+} = require('../../controllers/user-controller')
 
 // /api/users
 router
   .router('/')
-  .get()
+  .get(getAllUsers)
+  .post(createUser);
+
+router
+  .route('/:id')
+  .get(getUserById)
+  .put(updateUser)
+  .delete(deleteUser);
+
+router
+  .route('/:userId/freinds/:friendId')
+  .post(addFriend)
+  .delete(deleteFriend)
